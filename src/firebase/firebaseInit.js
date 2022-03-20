a9d4d60cc653852176da1c2284359d7f4f07762f
Marian Pop <35864222+mvpopuk@users.noreply.github.com>
2022-03-21 01:52:46 +0200

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 // Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyB0UOV04TIrPsSGh6bjJq-Y6MXKvvaKHao",
-    authDomain: "invoicer-2cf57.firebaseapp.com",
-    projectId: "invoicer-2cf57",
-    storageBucket: "invoicer-2cf57.appspot.com",
-    messagingSenderId: "1080173950171",
-    appId: "1:1080173950171:web:b7c08c4ecd7cdd24f488fe"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
